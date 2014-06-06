dba94e96dde730159587330f21d602e951f75901
Cid Dennis <cid.dennis@gmail.com>
2014-06-06 09:59:45 -0600

    Wrong Runit for Redis

--- bndl-stage/recipes/default.rb
+++ bndl-stage/recipes/default.rb
@@ -1,49 +1,49 @@
-group "postgres" do
-  action :create
-  system true
-end
+# group "postgres" do
+#   action :create
+#   system true
+# end
 
-user "postgres" do
-  comment "Postgres User"
-  home    "/home/postgres"
-  shell   "/bin/bash"
-  gid     "postgres"
-  supports :manage_home => false
-  action  :create
-  system true
-end
+# user "postgres" do
+#   comment "Postgres User"
+#   home    "/home/postgres"
+#   shell   "/bin/bash"
+#   gid     "postgres"
+#   supports :manage_home => false
+#   action  :create
+#   system true
+# end
 
-# FIX: Work around the fact that Chef creates the directory even for `manage_home: false`
-bash "remove the postgres user home" do
-  user    'root'
-  code    "rm -rf  /home/postgres"
-  not_if  { ::File.symlink?("/home/redis") } 
-  only_if { ::File.directory?("/home/redis") }
-end
+# # FIX: Work around the fact that Chef creates the directory even for `manage_home: false`
+# bash "remove the postgres user home" do
+#   user    'root'
+#   code    "rm -rf  /home/postgres"
+#   not_if  { ::File.symlink?("/home/redis") } 
+#   only_if { ::File.directory?("/home/redis") }
+# end
 
 
-group "redis" do
-  action :create
-  system true
-end
+# group "redis" do
+#   action :create
+#   system true
+# end
 
-user "redis" do
-  comment "redis User"
-  home    "/home/redis"
-  shell   "/bin/bash"
-  gid     "redis"
-  supports :manage_home => false
-  action  :create
-  system true
-end
+# user "redis" do
+#   comment "redis User"
+#   home    "/home/redis"
+#   shell   "/bin/bash"
+#   gid     "redis"
+#   supports :manage_home => false
+#   action  :create
+#   system true
+# end
 
-# FIX: Work around the fact that Chef creates the directory even for `manage_home: false`
-bash "remove the redis user home" do
-  user    'root'
-  code    "rm -rf /home/redis"
-  not_if  { ::File.symlink?("/home/redis") } 
-  only_if { ::File.directory?("/home/redis") }
-end
+# # FIX: Work around the fact that Chef creates the directory even for `manage_home: false`
+# bash "remove the redis user home" do
+#   user    'root'
+#   code    "rm -rf /home/redis"
+#   not_if  { ::File.symlink?("/home/redis") } 
+#   only_if { ::File.directory?("/home/redis") }
+# end
 
 
 group "sidekiq" do
@@ -72,17 +72,17 @@ end
 
 
 
-# Create service
-#
-template "/etc/init/postgres.conf" do
-  source "postgres-init.conf.erb"
-  owner 'root' and mode 0755
-end
+# # Create service
+# #
+# template "/etc/init/postgres.conf" do
+#   source "postgres-init.conf.erb"
+#   owner 'root' and mode 0644
+# end
 
-template "/etc/init/redis.conf" do
-  source "redis-init.conf.erb"
-  owner 'root' and mode 0755
-end
+# template "/etc/init/redis.conf" do
+#   source "redis-init.conf.erb"
+#   owner 'root' and mode 0644
+# end
 
 
 # service "elasticsearch" do
