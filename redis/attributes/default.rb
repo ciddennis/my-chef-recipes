c0f9f883b5301806c3f836577c5bb15074340fcd
Cid Dennis <cid.dennis@gmail.com>
2014-06-06 10:08:23 -0600

    Wrong version of redid

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -26,8 +26,8 @@ default[:redis][:server][:port]     = "6379"
 # Install
 #
 
-default[:redis][:version]           = "2.0.2"
-default[:redis][:release_url]       = "http://redis.googlecode.com/files/redis-:version:.tar.gz"
+default[:redis][:version]           = "2.8.10"
+default[:redis][:release_url]       = "http://download.redis.io/releases/redis-:version:.tar.gz"
 
 #
 # Tunables
