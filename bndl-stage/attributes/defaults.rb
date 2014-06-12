e9142f7a5d0eed9b07b65a1b9984a00864ac8322
Cid Dennis <cid.dennis@gmail.com>
2014-06-12 08:30:26 -0600

    Maybe if I read how to do this shit it might help.

--- bndl-stage/attributes/defaults.rb
+++ bndl-stage/attributes/defaults.rb
@@ -1,13 +1,13 @@
 node.default[:logstash] ||= {:agent => {}}
 
 
-node[:logstash][:agent][:user] = "logstash"
-node[:logstash][:agent][:group] = "logstash"
-node[:logstash][:agent][:source_url] = "https://download.elasticsearch.org/logstash/logstash/logstash-1.4.1.tar.gz"
-node[:logstash][:agent][:checksum] = "834599d28ce50012c221ece7a6783c5943221e36"
-node[:logstash][:agent][:version] = "1.4.1"
+default.logstash[:agent][:user] = "logstash"
+default.logstash[:agent][:group] = "logstash"
+default.logstash[:agent][:source_url] = "https://download.elasticsearch.org/logstash/logstash/logstash-1.4.1.tar.gz"
+default.logstash[:agent][:checksum] = "834599d28ce50012c221ece7a6783c5943221e36"
+default.logstash[:agent][:version] = "1.4.1"
 
-node[:logstash][:agent][:inputs] = [
+default.logstash[:agent][:inputs] = [
           {
               "redis" => {
                   "codec" => "json",
@@ -17,7 +17,7 @@ node[:logstash][:agent][:inputs] = [
           }
         ]
 
-node[:logstash][:agent][:outputs] = [ {
+default.logstash[:agent][:outputs] = [ {
               "elasticsearch" => {
                 "host" => "localhost"
               }
